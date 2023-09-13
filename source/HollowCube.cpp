335c4269e9aea43d3b6d207866e5997a31122e34
intotito <intotito@gmail.com>
2023-09-13 09:46:50 +0100

    All vertices generated
    Pending generation of indices, HollowCube is ready to draw

--- source/HollowCube.cpp
+++ source/HollowCube.cpp
@@ -13,29 +13,53 @@ HollowCube::~HollowCube()
 
 void HollowCube::GenerateMesh()
 {
+	segments = 8;
 	float h = 0.5f;
 	float w = 0.5f;
-	float z = 0.0f;
+	float z = 0.5f;
+	float r = 0.25f;
 	float phi = (360.0f / segments);
 	float ratio_sum = (90.0f / phi);
-	float ratios[2] = {glm::ceil(ratio_sum / 2.0f), ratio_sum - glm::ceil(ratio_sum / 2.0f)};
+	float ratios[2] = { glm::ceil( glm::ceil(ratio_sum) / 2.0f), ratio_sum - glm::ceil(glm::ceil(ratio_sum) / 2.0f) };
 	std::cout << "Ratios: {" << ratios[0] << ", " << ratios[1] << "}" << std::endl;
+
+
+
+
+	int n_Vertices = segments * 2 * 2;
+	int n_Faces = 4 * segments;
+
+	std::vector<vec3> pos;
+	std::vector<vec3> normal;
+	std::vector<vec2> texCoord;
+	float t = 0.0f;
+
 	float A[] = {
-		w, z, 
-		z, h,
-		-w, z,
-		z, -h
+		 w,		 0.0f,	
+		 0.0f,	 h,
+		-w,		 0.0f,
+		 0.0f,	 -h
+	};
+
+	glm::vec2 P[] = {
+		glm::vec2( w,	  0.0f	),	glm::vec2( w,		 h		),	
+		glm::vec2( 0.0f,  h		),	glm::vec2(-w,		 h		),
+		glm::vec2(-w,	  0.0f	),	glm::vec2(-w,		-h		),
+		glm::vec2( 0.0f, -h		),	glm::vec2( w,		-h		)
 	};
 
 	glm::vec2 T[] = {
-		glm::vec2(0.0f, 1.0f), glm::vec2(-1.0f, 0.0f), 
-		glm::vec2(-1.0f, 0.0f), glm::vec2(0.0f, -1.0f), 
-		glm::vec2(0.0f, -1.0f), glm::vec2(1.0f, 0.0f), 
-		glm::vec2(1.0f, 0.0f), glm::vec2(0.0f, 1.0f)
+		glm::vec2( 0.0f,  h		),	glm::vec2(-w,		 0.0f	), 
+		glm::vec2(-w,	  0.0f	),	glm::vec2( 0.0f,	-h		), 
+		glm::vec2( 0.0f, -h		),	glm::vec2( w,		 0.0f	), 
+		glm::vec2( w,	  0.0f	),	glm::vec2( 0.0f,	 h		)
 	};
-	
+	float x = 0.0f;
+	float y = 0.0f;
+
 	for (int i = 0; i < segments; i++)
 	{
+		
 		float theta = i * phi;
 		int quadrant = glm::floor(theta / 90.0f) + 1;
 		float target = 0.0f;
@@ -49,15 +73,58 @@ void HollowCube::GenerateMesh()
 			target = start + ((ratios[(quadrant + 1) % 2] / ratio_sum) * 90.0f);
 			ratio = (theta - start) / (target - start);
 			branch = 0;
+			x = P[(quadrant - 1) * 2].x + T[(quadrant - 1) * 2].x * ratio;
+			y = P[(quadrant - 1) * 2].y	+ T[(quadrant - 1) * 2].y * ratio;
 		}
 		else {
 			start = 90.0f * (quadrant - 1) + (ratios[(quadrant + 1) % 2] / ratio_sum * 90.0f);
 			target = start + ((ratios[quadrant % 2] / ratio_sum) * 90.0f);// 90.0f * quadrant;
 			ratio = (theta - start) / (target - start);
 			branch = 1;
+			x = P[(quadrant - 1) * 2 + 1].x		+ T[(quadrant - 1) * 2 + 1].x * ratio;
+			y = P[(quadrant - 1) * 2 + 1].y		+ T[(quadrant - 1) * 2 + 1].y * ratio;
 		}
+		float kx = 1.0f / w / 2.0f;
+		float ky = 1.0f / h / 2.0f;
+
+		pos.push_back(vec3(x, y, z));
+		normal.push_back(vec3(0.0f, 0.0f, 1.0f));
+		texCoord.push_back(vec2(kx * (w + x), ky * (h + y)));
+		t = 0.0f;
 
 		std::cout << "i : " << i << " phi:" << phi << " quadrant: " << quadrant << " Theta: " << theta << "\tStart: " << 
-			start << "\tTarget: " << target << "\tBranch: " << branch << std::endl;
+			start << "\tTarget: " << target << "\tRatio: " << ratio << "\tBranch: " << branch
+			<< "\tx: " << x << "\ty: " << y << std::endl;
+	}
+	vector<vec3> position(pos.size() * 4);
+	vector<vec3> norm(segments * 4);
+	
+	float angle = 0.0f;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < segments; j++)
+		{
+			float theta = j * phi;
+			x = r * glm::cos(glm::radians(theta));
+			y = r * glm::sin(glm::radians(theta));
+			vec3 coord = glm::rotate(mat4(1.0f), glm::radians(angle), vec3(1.0f, 0.0f, 0.0f)) * vec4(x, y, z, 1.0f);
+			vec3 posi = glm::rotate(mat4(1.0f), glm::radians(angle), vec3(1.0f, 0.0f, 0.0f)) * vec4(pos[j], 1.0f);
+			vec3 nor = glm::rotate(mat4(1.0f), glm::radians(angle), vec3(1.0f, 0.0f, 0.0f)) * vec4(normal[j], 1.0f);
+			position[(i * segments * 2) + j] = coord;
+			position[((i + 1) * segments) + j]	= posi;
+			norm[(i * segments * 2) + j] = nor;
+			norm[((i + 1) * segments) + j] = nor;
+		}
+		angle += 180.0f;
+	}
+	int stride = sizeof(Vertex) / sizeof(float);
+	float* data = new float[n_Faces * 4];
+
+	for (int i = 0; i < segments; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+
+		}
 	}
 }
